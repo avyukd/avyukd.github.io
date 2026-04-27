c2131b06933753e3c7044b65268d71820ffef632
avyukd <avyukd@gmail.com>
2026-04-26 17:00:11 -0700

    feat(quartz): move Backlinks to afterBody so topic stubs aren't visually empty

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [Component.Backlinks()],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -47,7 +47,6 @@ export const defaultContentPageLayout: PageLayout = {
       globalGraph: { drag: true, zoom: true, depth: -1 },
     }),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
   ],
 }
 
